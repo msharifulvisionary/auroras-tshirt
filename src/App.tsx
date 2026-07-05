6c71f0023b1a8fd50009d3d88c4da16902cd3f1b
MD SHARIFUL ISLAM <94898754+msharifulvisionary@users.noreply.github.com>
2026-07-05 23:45:41 +0600

    Add main application layout with routing

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import Home from './pages/Home';
+import ApplicationForm from './pages/ApplicationForm';
+import AdminLogin from './pages/AdminLogin';
+import AdminDashboard from './pages/AdminDashboard';
+import TrackStatus from './pages/TrackStatus';
+import ScrollToTop from './components/ScrollToTop';
+
+function Layout() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <ScrollToTop />
+      <Navbar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="apply" element={<ApplicationForm />} />
+          <Route path="track" element={<TrackStatus />} />
+          <Route path="admin" element={<AdminLogin />} />
+          <Route path="admin/dashboard" element={<AdminDashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
